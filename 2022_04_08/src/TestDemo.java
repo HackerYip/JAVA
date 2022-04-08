ddfc943f7aed9d4dad333738b858a96cd560efb2
CODE&HOT <975231661@qq.com>
2022-04-08 15:06:34 +0800

    20220408

--- 2022_04_08/src/TestDemo.java
+++ 2022_04_08/src/TestDemo.java
@@ -1,12 +1,14 @@
+import java.sql.SQLOutput;
 import java.util.Locale;
 import java.util.Scanner;
 
 public class TestDemo {
 
 
+    private static int count = 0;//全局，记分;
 
     public static void main(String[] args) {
-        String [][] map = new String[7][5];
+        String [][] map = new String[10][12];
         //初始化
         for(int i = 0; i < map.length; i++){
             //第一个数组
@@ -14,29 +16,35 @@ public class TestDemo {
 
                 double rand = Math.random();
                 if(rand < 0.2){
-                    map[i][j] = "#";//豆子，分
+                    map[i][j] = "★";//豆子，分
                 }
                 else if(rand < 0.5) {
-                    map[i][j] = "S";
+                    map[i][j] = "※";//扣分
                 }
                 else if(rand < 0.6){
                     map[i][j] = "B";//炸弹
                     }
-
                 else{
-                    map[i][j] = "*";//空白
+                    map[i][j] = "□";//空白
                 }
-
             }
         }
         int playerX = (int)(Math.random() * map[0].length);
         int playerY = (int)(Math.random() * map[0].length);
 
-        map[playerY][playerX] = "1";
+        int portalX =(int)(Math.random() * map[0].length);
+        int portalY =(int)(Math.random() * map[0].length);
+
+        int portalX2 =(int)(Math.random() * map[0].length);
+        int portalY2=(int)(Math.random() * map[0].length);
+
+        map[portalX][portalY] = "P";
+        map[portalX2][portalY2] = "P";
+
 
         for(int y = 0; y < map.length; y++){
             for(int x = 0; x < map[0].length; x++){
-                System.out.print(map[y][x] + " ");
+                System.out.printf("%-2s\t",map[y][x]);
             }
 
             System.out.println();
@@ -83,31 +91,50 @@ public class TestDemo {
                         }
                     break;
             }
-            int count = 0;
-            count = Judge(targetX,targetY,map,count);
+            int ret = 0;
+            ret = Judge(targetX,targetY,map);
             targetX = playerX;
             targetY = playerY;
 
-            System.out.println("分数：" + count);
+            switch(ret){
+                case 1:
+                    System.out.println("分数：" + (++count));
+                    break;
+                case 2:
+                    System.out.println("分数：" + (--count));
+                    break;
+                case 3:
+                    count = 0;
+                    System.out.println("分数:" + count);
+                    break;
+                case 4:
+
+
+
+
+
+
+
+                    break;
+
+
+            }
+
         }while(true);
 
     }
 
-    public static int Judge(int targetX, int targetY,String[][] map,int ret) {
+    public static int Judge(int targetX, int targetY,String[][] map) {
         String str = map[targetY][targetX];
         if (str.equals("#")) {
-            ret++;
+            return 1;
+        } else if (str.equals("S")) {
+            return 2;
+        } else if (str.equals("B")) {
+            return 3;
+        } else  {
+            return 4;
         }
-        if (str.equals("S")) {
-            return ret--;
-        }
-        if (str.equals("B")) {
-
-        }
-
-
     }
-
 }
-
 
