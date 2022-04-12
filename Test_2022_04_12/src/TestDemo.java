2936abf2527b4eb63e84c67e8a9d7d31eaf3744e
CODE&HOT <975231661@qq.com>
2022-04-12 14:30:28 +0800

    20220412数组

--- Test_2022_04_12/src/TestDemo.java
+++ Test_2022_04_12/src/TestDemo.java
@@ -1,7 +1,25 @@
 public class TestDemo {
 
+    //写一个函数， 数组以字符串的形式进行输出[1,2,3,4]
+
+    public static String myToString(int[] array){
+        String str = "[";
+        for (int i = 0; i < array.length; i++) {
+            str = str + array[i];
+            if(i != array.length - 1){
+                str += ",";
+            }
+        }
+                str += ",";
+                return str;
+    }
+
+
+
     public static void main(String[] args) {
         int[] array = {1,2,3,4};
+        String ret = myToString(array);
+        System.out.println(ret);
     }
 
 }
