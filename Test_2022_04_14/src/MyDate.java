bfbc88a641318248212e07c3383c204c11801739
CODE&HOT <975231661@qq.com>
2022-04-14 13:58:37 +0800

    20220414面向对象

--- Test_2022_04_14/src/MyDate.java
+++ Test_2022_04_14/src/MyDate.java
@@ -7,13 +7,13 @@ public class MyDate {
 
     //设置日期
 
-    public void setDate(int myYear,int myMonth,int myDay){
-        year = myYear;
-        month = myMonth;
-        day = myDay;
+    public void setDate(MyDate this,int year,int month,int day){
+        this.year = year;//用this区分这个被赋值的这个year不是形参的year
+        this.month = month;//加上this就是当前对象的month，当前是哪个对象？谁调用setDate，当前对象就是谁
+        this.day = day;
     }
 
-    public void printDate(){
+    public void printDate(MyDate this){
         System.out.println("年 "+ year + "月 "+ month + "日 "+day);
     }
 
