8af67937e1f31c8c531316881002ed40da7340f5
CODE&HOT <975231661@qq.com>
2022-04-15 20:32:50 +0800

    20220415类和对象

--- Test_2022_04_15/src/com/bit/demo3/TestDemo.java
+++ Test_2022_04_15/src/com/bit/demo3/TestDemo.java
@@ -6,7 +6,7 @@ class Student{
     private String name;//没有强制规定什么时候用private或者public
     private int age;
     private double score;
-    private static String classes;
+    public static String classes;
 
 
     public void doClass(){
@@ -50,6 +50,7 @@ class Student{
                 "name='" + name + '\'' +
                 ", age=" + age +
                 ", score=" + score +
+                "班级 =" + classes+
                 '}';
     }
 }
@@ -59,6 +60,10 @@ public class TestDemo {
         Student student1 = new Student("bit",18,34.6);
         Student student2 = new Student("张三",28,14.6);
 
-        System.out.println();
+        Student.classes = "104Java";
+        System.out.println(Student.classes);//静态变量不需要实例化对象
+        //student1.classes = "hhhhhh";//这个是反例，不建议这么写，这个classes是属于类变量，不属于某一个对象的
+        //静态的成员变量和静态的成员方法，都不依赖于对象来调用，因为他们是类变量，通过类名来访问的
+        //静态的成员方法当中，不能访问非静态的成员（包括成员变量和成员方法）。非静态的成员变量依赖对象
     }
 }
