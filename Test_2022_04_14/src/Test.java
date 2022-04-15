10389c46f548c23f6db9eb9e0745ead8173d492f
CODE&HOT <975231661@qq.com>
2022-04-15 17:06:41 +0800

    20220415类和对象

---
+++ Test_2022_04_14/src/Test.java
@@ -0,0 +1,14 @@
+public class Test {
+        public int aMethod(){
+            int i = 0;
+            i++;
+            return i;
+        }
+        public static void main(String args[]){
+            Test test = new Test();
+            test.aMethod();
+            int j = test.aMethod();
+            System.out.println(j);
+        }
+    }
+
---
+++ Test_2022_04_15/src/TestDemo.java
@@ -0,0 +1,26 @@
+
+class Person{
+    private String name;
+    private int age;
+
+    //私有的
+    //public 访问修饰限定符
+    //被修饰的 只是在访问 权限上不同
+    // 类外别人拿不到成员变量 还有成员方法
+}
+
+
+
+
+
+
+
+public class TestDemo {
+    // 封装
+    public static void main(String[] args) {
+        Person person = new Person();
+       // person.name;
+
+    }
+
+}
