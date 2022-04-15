9901d44b7e8b717020d27c31e2603d27f2eca25b
CODE&HOT <975231661@qq.com>
2022-04-15 17:25:41 +0800

    20220415类和对象

--- Test_2022_04_15/src/TestDemo.java
+++ Test_2022_04_15/src/TestDemo.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 
 class Person{
     private String name;
@@ -7,6 +8,16 @@ class Person{
     //public 访问修饰限定符
     //被修饰的 只是在访问 权限上不同
     // 类外别人拿不到成员变量 还有成员方法
+
+    //封装: 对类的实现的细节进行隐藏，对外只提供公开的方法来供你使用。提高了对数据的安全性
+
+    String sex;//默认的:包访问权限(什么也没加在前面)
+    //什么是包？
+    //为了更好的管理类，把多个类收集在一起成为一组，称为软件包
+
+    //import java.util.Arrays;
+    //相当于C的include
+    //
 
 
     private void sleep(){
@@ -32,10 +43,20 @@ class Person{
 
 public class TestDemo {
     // 封装
+
     public static void main(String[] args) {
+        int[] array = {1,2,3};
+        System.out.println(Arrays.toString(array));
+    }
+
+
+
+
+    public static void main2(String[] args) {
         Person person = new Person();
        // person.name;
         person.setName("关羽");//所以区别于上一行代码，通过公开接口访问成员变量name
+        person.eat();
     }
 
 }
