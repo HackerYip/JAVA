92aa44c6e8d17ac3b4cbd4cdcd57ec824fad0c65
CODE&HOT <975231661@qq.com>
2022-04-15 17:10:15 +0800

    20220415类和对象

--- Test_2022_04_15/src/TestDemo.java
+++ Test_2022_04_15/src/TestDemo.java
@@ -7,6 +7,21 @@ class Person{
     //public 访问修饰限定符
     //被修饰的 只是在访问 权限上不同
     // 类外别人拿不到成员变量 还有成员方法
+
+
+    private void sleep(){
+        System.out.println("睡觉");
+    }
+
+
+    public void eat(){
+        this.sleep();//所以此处可以提供一个公开接口来访问private方法
+    }
+
+
+    public void setName(String name){
+        this.name =  name;
+    }
 }
 
 
@@ -20,7 +35,7 @@ public class TestDemo {
     public static void main(String[] args) {
         Person person = new Person();
        // person.name;
-
+        person.setName("关羽");//所以区别于上一行代码，通过公开接口访问成员变量name
     }
 
 }
