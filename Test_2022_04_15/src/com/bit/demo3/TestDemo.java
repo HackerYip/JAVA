b7043a3ad30136c82411a4c9a980d5bf9c16bdcd
CODE&HOT <975231661@qq.com>
2022-04-15 22:13:41 +0800

    20220415类和对象

--- Test_2022_04_15/src/com/bit/demo3/TestDemo.java
+++ Test_2022_04_15/src/com/bit/demo3/TestDemo.java
@@ -14,6 +14,20 @@ public class Test {
     }
 }
 
+public class Test {
+    public int aMethod(){
+        static int i = 0;
+        i++;
+        return i;
+    }
+    public static void main(String args[]){
+        Test test = new Test();
+        test.aMethod();
+        int j = test.aMethod();
+        System.out.println(j);
+    }
+}
+
 class Test {
     public static void hello() {
         System.out.println("hello");
