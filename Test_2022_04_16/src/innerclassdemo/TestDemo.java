46ea379df5a85f363a4761e91380aeddd92784ad
CODE&HOT <975231661@qq.com>
2022-04-16 19:25:54 +0800

    20220416内部类+继承

--- Test_2022_04_16/src/innerclassdemo/TestDemo.java
+++ Test_2022_04_16/src/innerclassdemo/TestDemo.java
@@ -9,15 +9,32 @@ class OuterClass{
 
     class InnerClass{
         //实例内部类
-        //如何实例化内部类对象
+        //1.如何实例化内部类对象
+        //外部类命.内部类名（这个时候才能拿到内部类的类型） 变量 = 外部类对象的引用.new InnerClass()
+
+        //2.实例内部类当中不能定义静态的成员变量？ 但是要定义， 必须是编译的时候确定的值（必须是static final的）
+        //public static final int data6 = 60;
+        //常量 在编译的时候就确定了这个值是几了
+
+        //3.实例内部类当中，如果有成员变量和外部类的成员变量重名了，怎么办？
+        //优先使用自己的！
+        //如何拿到外部类的呢？
+        //OuterClass.this.对象
+        //实例内部类当中，不仅包含了自己的this，而且包含了外部类的this
+
         public int data4 = 40;
         private int data5 = 50;
+
         //public static int data6 = 60;
+        public static final int data6 = 60;
 
         public InnerClass(){
             System.out.println("InnerClass的构造方法");
         }
         public void method(){
+            System.out.println(data1);
+            System.out.println(data2);
+            System.out.println(data3);
             System.out.println("InnerClass的一个method方法! ");
         }
     }
