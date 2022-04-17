61c0409787becae7ac77b69072f593ab887e2f83
CODE&HOT <975231661@qq.com>
2022-04-18 00:25:15 +0800

    20220418多态+接口

--- Test_2022_04_18/src/demo4/Test.java
+++ Test_2022_04_18/src/demo4/Test.java
@@ -1,6 +1,7 @@
 package demo4;
 
 import java.util.Arrays;
+import java.util.Comparator;
 
 class Student implements Comparable<Student> {
     public String name;
@@ -40,9 +41,54 @@ class Student implements Comparable<Student> {
     }
 }
 
+
+
+class Student2{
+    public String name;
+    public int age;
+    public double score;
+
+    public Student2(String name, int age, double score) {
+        this.name = name;
+        this.age = age;
+        this.score = score;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", score=" + score +
+                '}';
+    }
+
+    //比较器
+    class AgeComparator implements Comparable<Student>, Comparator<T> {
+
+        @Override
+        public int compareTo(Student o1,Student o2) {
+            return o1.age-o2.age;
+        }
+    }
+
 public class Test {
 
+
     public static void main(String[] args) {
+        Student[] students = new Student[3];
+        students[0] = new Student("zhangsan",18,78.9);
+        students[1] = new Student("lisi",38,18.9);
+        students[2] = new Student("abc",78,88.9);
+
+        AgeComparator ageComparator = new AgeComparator();
+
+        Arrays.sort(students,ageComparator);//可以传第二个参数，比较器，决定排序类型
+        System.out.println(Arrays.toString(students));
+    }
+}
+
+    public static void main1(String[] args) {
         Student student1 = new Student("zhangsan",18,78.9);
 
         Student student2 = new Student("lisi",38,18.9);
