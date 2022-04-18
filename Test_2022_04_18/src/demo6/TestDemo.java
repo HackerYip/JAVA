f2e41f4890ac274a64ee00f3a325f3e57678d2e8
CODE&HOT <975231661@qq.com>
2022-04-18 23:04:31 +0800

    20220418多态+接口

--- Test_2022_04_18/src/demo6/TestDemo.java
+++ Test_2022_04_18/src/demo6/TestDemo.java
@@ -10,7 +10,10 @@ class Person implements Cloneable{
                 '}';
     }
 
-
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
 }
 
 
@@ -18,6 +21,8 @@ public class TestDemo {
     //1.这个对象是可以被克隆的
     //2.
     Person person1 = new Person();
-    Person person2 = person1.cl
+    Person person2 = person1.clone();//这里clone()的返回值是Object类的，是一个父类，把父类给了子类，要强转
 
+    public TestDemo() throws CloneNotSupportedException {
+    }
 }
