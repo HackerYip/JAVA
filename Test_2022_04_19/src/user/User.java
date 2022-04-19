be899ffd17d3ae7a6c017f08d712db5e176ae990
CODE&HOT <975231661@qq.com>
2022-04-19 20:26:56 +0800

    20220419接口object+练习

--- Test_2022_04_19/src/user/User.java
+++ Test_2022_04_19/src/user/User.java
@@ -4,9 +4,9 @@ public class User {
     protected String name;//共性写在这
     //防止在其他包也要继承到，所以用protect
 
- public User(String name){
-     this.name = name;
- }
+    public User(String name){
+        this.name = name;
+    }
 
     public void menu(){
 
