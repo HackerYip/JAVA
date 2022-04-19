7fa6325fb1c4118b281989315d6f5951f9f12c5a
CODE&HOT <975231661@qq.com>
2022-04-19 19:55:34 +0800

    20220419接口object+练习

--- Test_2022_04_19/src/Main.java
+++ Test_2022_04_19/src/Main.java
@@ -1,17 +1,29 @@
+import user.AdminUser;
+import user.NormalUser;
+import user.User;
+
 import java.util.Scanner;
 
 public class Main {
 
-    public static void login(){
+    public static User login(){
         System.out.println("请输入姓名：");
         Scanner scanner = new Scanner(System.in);
         String name = scanner.nextLine();
         System.out.println("请输入你的身份: 1->管理员， 0->普通用户");
+        int choice = scanner.nextInt();
+        if(choice == 1){
+            return new AdminUser(name);
+        }
+        else{
+            return new NormalUser(name);
+        }
     }
 
 
     public static void main(String[] args) {
 
-        login();
+        User user = login();//user到底引用哪个对象，需要看
+        user.menu();
     }
 }
