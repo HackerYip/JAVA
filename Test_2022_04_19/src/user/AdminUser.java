32fb015592fc457a6ebb7dc569e4f89230249329
CODE&HOT <975231661@qq.com>
2022-04-19 19:27:36 +0800

    20220419接口object+练习

--- Test_2022_04_19/src/user/AdminUser.java
+++ Test_2022_04_19/src/user/AdminUser.java
@@ -5,4 +5,12 @@ public class AdminUser extends User{
     public AdminUser(String name) {
         super(name);
     }
+    public void menu(){
+        System.out.println("hello "+this.name+"欢迎来到图书小练习");
+        System.out.println("1.查找图书");
+        System.out.println("2.新增图书");
+        System.out.println("3.删除图书");
+        System.out.println("4.显示图书");
+        System.out.println("0.退出系统");
+    }
 }
--- Test_2022_04_19/src/user/NormalUser.java
+++ Test_2022_04_19/src/user/NormalUser.java
@@ -5,4 +5,11 @@ public class NormalUser extends User{
     public NormalUser(String name) {
         super(name);
     }
+    public void menu(){
+        System.out.println("hello "+this.name+"欢迎来到图书小练习");
+        System.out.println("1.查找图书");
+        System.out.println("2.借阅图书");
+        System.out.println("3.归还图书");
+        System.out.println("0.退出系统");
+    }
 }
