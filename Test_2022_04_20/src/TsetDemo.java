6856e7b12cd830c11f7affb65327a6a2847f7dae
CODE&HOT <975231661@qq.com>
2022-04-20 15:21:32 +0800

    20220420String

--- Test_2022_04_20/src/TsetDemo.java
+++ Test_2022_04_20/src/TsetDemo.java
@@ -1,7 +1,19 @@
 public class TsetDemo {
 
-
     public static void main(String[] args) {
+        int a = 10;
+        String str = String.valueOf(a);
+        System.out.println(str);//就会把10变成字符串10
+
+
+        String str2 = "1234";
+        int ret = Integer.valueOf(str2);
+        System.out.println(ret+1);
+    }
+
+
+
+    public static void main1(String[] args) {
         String str = "hello";
         System.out.println(str.charAt(0));
 
@@ -15,8 +27,6 @@ public class TsetDemo {
         String str2 = "abcbdbcbda";
         System.out.println(str.lastIndexOf('a'));
         System.out.println(str.lastIndexOf('a',4));//从fromIndex的位置开始从后往前找'a'
-
-
 
     }
 
