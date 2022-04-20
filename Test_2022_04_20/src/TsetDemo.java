4616bde452f067c0dcd62d3291b5158e6e29e586
CODE&HOT <975231661@qq.com>
2022-04-20 15:17:39 +0800

    20220420String

--- Test_2022_04_20/src/TsetDemo.java
+++ Test_2022_04_20/src/TsetDemo.java
@@ -7,6 +7,17 @@ public class TsetDemo {
 
         int index = str.indexOf('h');//无论有多少个h，从头到位找到第一个字符h，返回下标，-1代表没有这个字符
         System.out.println(index);
+
+        //indexof还重载了很多方法
+
+
+
+        String str2 = "abcbdbcbda";
+        System.out.println(str.lastIndexOf('a'));
+        System.out.println(str.lastIndexOf('a',4));//从fromIndex的位置开始从后往前找'a'
+
+
+
     }
 
 }
