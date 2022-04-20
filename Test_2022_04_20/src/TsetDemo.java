0f73e20cc80d95e7b49c26a30c826a8b4f307d6d
CODE&HOT <975231661@qq.com>
2022-04-20 15:32:51 +0800

    20220420String

--- Test_2022_04_20/src/TsetDemo.java
+++ Test_2022_04_20/src/TsetDemo.java
@@ -1,8 +1,13 @@
+import java.util.Arrays;
 import java.util.Locale;
 
 public class TsetDemo {
 
-
+    public static void main(String[] args) {
+        String s = "hello";
+        char[] chars = s.toCharArray();
+        System.out.println(Arrays.toString(chars));
+    }
 
 
 
