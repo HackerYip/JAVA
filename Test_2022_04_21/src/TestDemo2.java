b20dbd07adcd6f5b667f5f4385f6809016e4d2f6
CODE&HOT <975231661@qq.com>
2022-04-21 17:24:15 +0800

    20220421 异常

--- Test_2022_04_21/src/TestDemo2.java
+++ Test_2022_04_21/src/TestDemo2.java
@@ -7,12 +7,20 @@ public class TestDemo2 {
         try{
             System.out.println(array[9]);
             System.out.println("haha");
-        }catch (NullPointerException e){
+//        }catch(Exception e){
+//            e.printStackTrace();//此时编译器懵了，不管抛出啥异常编译器都能捕捉到
+//            //所以不能把父类放在最前头
+        }
+        catch (NullPointerException e){
             System.out.println("空指针异常");
             e.printStackTrace();
         }catch (ArrayIndexOutOfBoundsException e){
             System.out.println("数组越界异常");
             e.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();//要放在这里
+            //放在这里的意思就是
+            //
         }
     }
 }
