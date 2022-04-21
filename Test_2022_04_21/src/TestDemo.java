ca6f04b312e4445309528a02a8046946072481f8
CODE&HOT <975231661@qq.com>
2022-04-21 17:09:37 +0800

    20220421 异常

--- Test_2022_04_21/src/TestDemo.java
+++ Test_2022_04_21/src/TestDemo.java
@@ -1,7 +1,9 @@
 public class TestDemo {
 
     public static void func3(int a) throws CloneNotSupportedException{
-        if(a == 10){//这里不拿try catch处理异常的话，就要throw声明异常
+        if(a == 10){//这里不拿try catch处理异常的话，就要throws声明异常
+            //但是这里和下面的try catch不一样，这里只是声明异常，处理了受查异常而已
+            //真正要在main函数调用func3的时候，同样会报错，因为没处理异常
             throw new CloneNotSupportedException("a==10");
         }
     }//
