34dbadfed85ac0e9ebb468bcc0a02b4d179b9609
CODE&HOT <975231661@qq.com>
2022-04-21 17:12:34 +0800

    20220421 异常

--- Test_2022_04_21/src/TestDemo.java
+++ Test_2022_04_21/src/TestDemo.java
@@ -18,18 +18,22 @@ public class TestDemo {
         }
     }//这里是自己处理的，所以下面main函数调用的时候，可以执行后面的打印业务，没有交给JVM处理异常
 
-    public static void main(String[] args) {
+    public static void main4(String[] args) {
         try{
             func3(10);
         }catch(CloneNotSupportedException e){
             e.printStackTrace();
-        }//所以这里来处理
+        }//所以这里来处理func3的异常
 
         func2(10);
         System.out.println("dasdasd");
     }
 
-
+    public static void main(String[] args) throws CloneNotSupportedException{
+        func3(10);
+        System.out.println("fafaf");
+        //这里还是没有处理异常，只是不会发生编译异常，但是还是交给了JVM处理
+    }
 
 
 
