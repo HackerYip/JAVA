433a74536aa5700b81e99c82d848be8b96919a6d
CODE&HOT <975231661@qq.com>
2022-04-22 23:16:02 +0800

    20220422 String练习

--- Test_2022_04_22/src/TestDemo.java
+++ Test_2022_04_22/src/TestDemo.java
@@ -68,4 +68,25 @@ public class TestDemo {
             }
         }
     }
+
+
+
+    int binarySearch(int[] array, int value) {
+
+        int begin = 0;
+
+        int end = array.length - 1;
+
+        while (begin <= end) {
+
+            int mid = begin + ((end-begin) / 2);
+            if (array[mid] < value)
+                begin = mid + 1;
+            else if (array[mid] > value)
+                end = mid - 1;
+            else
+                return mid;
+        }
+        return -1;
+    }
 }
