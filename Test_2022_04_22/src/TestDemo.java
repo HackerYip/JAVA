8959c2ba3c2b90dce527b6ca864397e842ca2956
CODE&HOT <975231661@qq.com>
2022-04-22 23:19:21 +0800

    20220422 String练习

--- Test_2022_04_22/src/TestDemo.java
+++ Test_2022_04_22/src/TestDemo.java
@@ -80,12 +80,15 @@ public class TestDemo {
         while (begin <= end) {
 
             int mid = begin + ((end-begin) / 2);
-            if (array[mid] < value)
+            if (array[mid] < value) {
                 begin = mid + 1;
-            else if (array[mid] > value)
+            }
+            else if (array[mid] > value){
                 end = mid - 1;
-            else
+            }
+            else {
                 return mid;
+            }
         }
         return -1;
     }
