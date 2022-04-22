c18d64429ec2c6591ee3b7490a538c6ab69b2508
CODE&HOT <975231661@qq.com>
2022-04-22 23:01:34 +0800

    20220422 String练习

--- Test_2022_04_22/src/TestDemo.java
+++ Test_2022_04_22/src/TestDemo.java
@@ -4,7 +4,7 @@ public class TestDemo {
         String str = new String("good");
         char[ ] ch = { 'a' , 'b' , 'c' };
         public static void main(String args[]){
-//            Example ex = new Example();
+            Example ex = new Example();
             ex.change(ex.str,ex.ch);
             System.out.print(ex.str + " and ");
             System.out.print(ex.ch);
