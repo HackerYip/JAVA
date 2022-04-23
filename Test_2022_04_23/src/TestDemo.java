d46c8221a0f7b6587e42d1c833a62efd52cd9aa1
CODE&HOT <975231661@qq.com>
2022-04-23 14:52:27 +0800

    20220423 异常+复杂度

--- Test_2022_04_23/src/TestDemo.java
+++ Test_2022_04_23/src/TestDemo.java
@@ -40,5 +40,28 @@ public class TestDemo {
 
 
     }
-
+    public static void main(String[] args) {
+        //有时候我们抛出的异常是存在父子类关系的
+        //所有的异常都继承于Exception
+        int[] array = {1,2,3,4};
+        try{
+            System.out.println(array[9]);
+            System.out.println("haha");
+//        }catch(Exception e){
+//            e.printStackTrace();//此时编译器懵了，不管抛出啥异常编译器都能捕捉到
+//            //所以不能把父类放在最前头
+        }
+        catch (NullPointerException e){
+            System.out.println("空指针异常");
+            e.printStackTrace();
+        }catch (ArrayIndexOutOfBoundsException e){
+            System.out.println("数组越界异常");
+            e.printStackTrace();
+        }catch(Exception e){
+            e.printStackTrace();//要放在这里，兜底
+            //放在这里的意思就是
+            //假设程序捕获不到上面所写的异常
+            //那么就在这一段代码里面拦住所有的异常
+        }
+    }
 }
