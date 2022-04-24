550e6127f664b1fdb4b732b482a74e2a292c6cb0
CODE&HOT <975231661@qq.com>
2022-04-24 15:35:43 +0800

    20220424 泛型+Arraylist

--- Test_2022_04_24/src/TestDemo.java
+++ Test_2022_04_24/src/TestDemo.java
@@ -4,9 +4,11 @@ public class TestDemo {
     //从代码上讲，就是对类型实现了参数化
 
     //目标：数组中可以存放任何类型的数据 -> 我们知道，Object是所有类的父类
-    class MyArray{
+    class MyArray<T>{//占位符，代表当前类是一个泛型类
+        //这时候数组里面不能是Object
         public int[] array = new int[10];
         public Object[] array2 = new Object[10];
+        public T[] array3 = (T[])new Object[10];//这个写法也不好
 
     }
 
