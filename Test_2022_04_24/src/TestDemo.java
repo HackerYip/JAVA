56d66f8aae21204e096a7419af0f8238c1bb348e
CODE&HOT <975231661@qq.com>
2022-04-24 18:32:36 +0800

    20220424 泛型+Arraylist

--- Test_2022_04_24/src/TestDemo.java
+++ Test_2022_04_24/src/TestDemo.java
@@ -1,18 +1,34 @@
 public class TestDemo {
 
-     //泛型适用于许多许多类型
-    //从代码上讲，就是对类型实现了参数化
+//     //泛型适用于许多许多类型
+//    //从代码上讲，就是对类型实现了参数化
+//
+//    //目标：数组中可以存放任何类型的数据 -> 我们知道，Object是所有类的父类
+//    class MyArray<T>{//占位符，代表当前类是一个泛型类
+//        //这时候数组里面不能是Object
+//        public int[] array = new int[10];
+//        public Object[] array2 = new Object[10];
+//        public T[] array3 = (T[])new Object[10];//这个写法也不好
+//
+//    }
+//
+//    public T getPos(int pos){
+//        return array2[pos];
+//    }
+//
 
-    //目标：数组中可以存放任何类型的数据 -> 我们知道，Object是所有类的父类
-    class MyArray<T>{//占位符，代表当前类是一个泛型类
-        //这时候数组里面不能是Object
-        public int[] array = new int[10];
-        public Object[] array2 = new Object[10];
-        public T[] array3 = (T[])new Object[10];//这个写法也不好
+    public class MyArraylist{
+        public int[] elem;
+        public int usedSize;
 
-    }
+        public MyArraylist(){
+            this.elem = new int[10];
+        }
 
-    public T getPos(int pos){
-        return array2[pos];
+        //打印顺序表
+        public void display(){
+
+        }
+
     }
 }
