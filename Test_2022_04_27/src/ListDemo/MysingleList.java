3b452d40240deb53ab3d2c244d8e76a58ba9984f
CODE&HOT <975231661@qq.com>
2022-04-27 19:46:37 +0800

    20220427 扑克牌+链表

---
+++ Test_2022_04_27/src/ListDemo/MysingleList.java
@@ -0,0 +1,15 @@
+package ListDemo;
+
+public class MysingleList {
+
+    //链表(单向，双向，带头，不带头，循环，非循环)
+    //节点
+    //1.数值域，当前节点的地址
+    //2.next域，下一个节点的地址
+
+
+
+    static class ListNode{
+
+    }
+}
