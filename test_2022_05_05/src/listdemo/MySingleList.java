3bdd03bc2b52e1c9c46f803152f71f513c2b9bb8
CODE&HOT <975231661@qq.com>
2022-05-06 23:27:35 +0800

    20220506链表OJ

--- test_2022_05_05/src/listdemo/MySingleList.java
+++ test_2022_05_05/src/listdemo/MySingleList.java
@@ -28,6 +28,15 @@ public class MySingleList {
         listNode4.next = listNode5;
         this.head = listNode1;
     }
+    public void display() {
+        ListNode cur = head;
+        while (cur != null) {
+            System.out.print(cur.val+" ");
+            cur = cur.next;
+        }
+        System.out.println();
+    }
+
 
 
 }
