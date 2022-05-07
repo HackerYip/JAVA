500ce60769fe86956827d57422cece4f88e951f3
CODE&HOT <975231661@qq.com>
2022-05-07 22:13:01 +0800

    20220507复习数据结构

---
+++ Test_2022_05_07/src/sqlist/MyArrayList.java
@@ -0,0 +1,30 @@
+package sqlist;
+
+public class MyArrayList {
+
+    public int[] elem;
+    public int usedSized;//0
+
+
+    public MyArrayList(){
+        this.elem = new int[10];
+    }
+
+    //打印顺序表
+    public void display(){
+        //usedSized == 0;
+        for (int i = 0; i < this.usedSized; i++) {
+            System.out.println(elem[i] + " ");
+        }
+    }
+
+    //新增元素，默认在数组最后新增
+    public void add(int data){
+        //先判断是不是满的
+    }
+
+    //判断当前顺序表是不是满的
+    public boolean isFull(){
+
+    }
+}
