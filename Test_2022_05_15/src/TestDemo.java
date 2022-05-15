eef982013b55e4a5ed63bb6c96c54cb80f69f042
CODE&HOT <975231661@qq.com>
2022-05-15 22:09:36 +0800

    20220515 复习

---
+++ Test_2022_05_15/src/TestDemo.java
@@ -0,0 +1,13 @@
+public class TestDemo {
+
+    public static void main(String[] args) {
+        Message<Integer> message = new Message<>();
+        message.setMessage(1);
+        fun(message);
+    }
+
+    public static void fun(Message<?> temp){
+        //temp.setMessage(1);
+
+    }
+}
