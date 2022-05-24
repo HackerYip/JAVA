7c8723196a38fe02ae82d65977aae47a7edc175e
CODE&HOT <975231661@qq.com>
2022-05-24 23:15:28 +0800

    20220524 队列

--- Test_2022_05_18/src/MyLinkedList.java
+++ Test_2022_05_18/src/MyLinkedList.java
@@ -13,5 +13,26 @@ public class MyLinkedList {
             }
         }
 
+    public Node head;
+    public Node last;
+    public int usedSize;
+
+
+    /**
+     * 出队 但是不删除
+     * @return
+     */
+    public int peek() {
+        if(isEmpty()) {
+            throw new RuntimeException("队列为空！");
+        }
+        return head.val;
+    }
+    public int size() {
+        return usedSize;
+    }
+    public boolean isEmpty() {
+        return usedSize == 0;
+    }
 
 }
