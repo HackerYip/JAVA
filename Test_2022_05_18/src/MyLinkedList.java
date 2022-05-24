67863e79ba38adb2d95cf712312a038aa836fcc0
CODE&HOT <975231661@qq.com>
2022-05-24 23:15:09 +0800

    20220524 队列

---
+++ Test_2022_05_18/src/MyLinkedList.java
@@ -0,0 +1,17 @@
+public class MyLinkedList {
+
+    /**
+     * @Author 12629
+     * @Description：
+     */
+
+        class Node {
+            public int val;
+            public Node next;
+            public Node(int val) {
+                this.val = val;
+            }
+        }
+
+
+}
