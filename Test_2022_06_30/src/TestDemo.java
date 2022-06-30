d1c5c504186625054b4b85ab32c20826a7381d37
CODE&HOT <975231661@qq.com>
2022-06-30 22:52:26 +0800

    20220630 搜索树

---
+++ Test_2022_06_30/src/TestDemo.java
@@ -0,0 +1,34 @@
+public class TestDemo {
+    class A {
+
+    }
+
+    class B extends A {
+
+    }
+
+    class C extends B {
+
+    }
+
+    public class Test {
+
+        public static void testQuickSort(int[] array) {
+            array = Arrays.copyOf(array, array.length);
+            long startTime = System.currentTimeMillis();
+            TestSort.quickSort(array);
+            long endTime = System.currentTimeMillis();
+            System.out.println("快速排序：" + (endTime - startTime));
+        }
+
+
+        public static void testShellSort(int[] array) {
+            array = Arrays.copyOf(array, array.length);
+            long startTime = System.currentTimeMillis();
+            TestSort.shellSort(array);
+            long endTime = System.currentTimeMillis();
+            System.out.println("希尔排序：" + (endTime - startTime));
+        }
+
+
+}
